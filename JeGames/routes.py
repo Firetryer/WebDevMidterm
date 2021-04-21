0225811e41a9d942f35cddf60076cf8a035826be
Rewel Cabiles <rewel.sicuan@gmail.com>
2021-04-21 22:49:03 +0800

    Basic routes added

--- JeGames/routes.py
+++ JeGames/routes.py
@@ -10,3 +10,19 @@ def index():
 @app.route("/browse_games")
 def browse_page():
     return render_template("game_browse.html")
+
+@app.route("/login")
+def login_page():
+    return render_template("signin.html")
+
+@app.route("/create_account")
+def create_account_page():
+    return render_template("signup.html")
+
+@app.route("/support")
+def support_page():
+    return render_template("support.html")
+
+@app.route("/game_page")
+def game_page():
+    return render_template("game_page.html")
