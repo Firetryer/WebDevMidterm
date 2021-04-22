a9a3161a8a53a2209a0b2d7a1fc35b1a8cfb4cee
Rewel Cabiles <rewel.sicuan@gmail.com>
2021-04-22 20:32:24 +0800

    Still trying to fix csrf errors

--- JeGames/__init__.py
+++ JeGames/__init__.py
@@ -10,8 +10,12 @@ import re
 
 
 
+SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/uploads'
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['CSRF_ENABLED'] = True
+app.config['CSRF_SESSION_KEY'] = SECRET_KEY
 if 'IS_HEROKU' in os.environ:
     uri = os.getenv("DATABASE_URL")  # or other relevant config var
     if uri.startswith("postgres://"):
@@ -19,8 +23,8 @@ if 'IS_HEROKU' in os.environ:
     app.config['SQLALCHEMY_DATABASE_URI'] = uri
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["db_uri"]
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+
+
 
 os.makedirs(os.path.join(app.instance_path, 'uploads'), exist_ok=True)
 
